bd050ea48802c398ab04c55938866ce28e9097ac
keyujin <kerbenjamin@gmail.com>
2018-01-27 15:39:28 -0500

    Revert "update to latest edit"
    This reverts commit cca27314f43e932c2aead99e832646905110c5aa.

--- viewer/models.py
+++ viewer/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
 class Imgs(models.Model):
     img = models.ImageField()
 
 class Target(models.Model):
-    orientation = models.CharField(max_length=10)
-    shape = models.CharField(max_length=10)
-    alphanumeric = models.CharField(max_length=10)
-    color = models.CharField(max_length=10)
+        orientation = models.CharField(max_length=10)
+        shape = models.CharField(max_length=10)
+        alphanumeric = models.CharField(max_length=10)
+        color = models.CharField(max_length=10)
+        image = models.TextField()
