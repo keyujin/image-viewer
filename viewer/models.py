6ab849298575e5bf0ddfb171d4ccd6201133d342
Benjamin Ker <kerbenjamin@gmail.com>
2018-02-13 02:54:56 -0500

    Switching Croppie -> Cropper (Croppie bug with data URLs)

--- viewer/models.py
+++ viewer/models.py
@@ -10,4 +10,4 @@ class Target(models.Model):
         shape = models.CharField(max_length=10)
         alphanumeric = models.CharField(max_length=10)
         color = models.CharField(max_length=10)
-        image = models.TextField()
+        image = models.ImageField()
